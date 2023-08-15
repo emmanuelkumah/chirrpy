9e40e84264cd8e43902d75d32639648b4513e389
Emmanuel Kumah <mailkumah@gmail.com>
2023-08-15 14:03:43 +0000

    Add speechRecogniton API

--- src/pages/Main-App.jsx
+++ src/pages/Main-App.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaMicrophone } from "react-icons/fa";
+import getSpeechRecognitionAPI from "../services/speechRecognition";
 
 const Main = () => {
-  return <div>Main-App</div>;
+  const recognition = getSpeechRecognitionAPI();
+  recognition.continous = true;
+  //interim result
+  recognition.interimResults = true;
+  //language
+  recognition.lang = "en-US";
+
+  const startSpeechRecognitionHandler = () => {
+    recognition.onstart = () => {
+      console.log("Speech recognition service has started");
+    };
+    recognition.start();
+
+    //get resutls
+    recognition.onresult = (e) => {
+      console.log(e.results[0][0].transcript);
+    };
+  };
+  const stopSpeechRecognitionHandler = () => {
+    recognition.stop();
+  };
+
+  return (
+    <>
+      <section>
+        <h2>Record voice note</h2>
+        <section>
+          <p>Saved Notes here </p>
+        </section>
+        <section>
+          <p>Click on the microphone icon to startrecording</p>
+
+          <button
+            className="flex bg-red-500 py-2 px-3 gap-2 rounded-lg"
+            onClick={startSpeechRecognitionHandler}
+          >
+            Start recording
+            <FaMicrophone />
+          </button>
+          <button onClick={stopSpeechRecognitionHandler}>
+            Stop recogniton
+          </button>
+          <div>Recording content goes here</div>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Main;
---
+++ src/services/speechRecognition.js
@@ -0,0 +1,10 @@
+export default function getSpeechRecognitionAPI() {
+  const grammar =
+    "#JSGF V1.0; grammar colors; public <color> = aqua | azure | beige | bisque | black | blue | brown | chocolate | coral | crimson | cyan | fuchsia | ghostwhite | gold | goldenrod | gray | green | indigo | ivory | khaki | lavender | lime | linen | magenta | maroon | moccasin | navy | olive | orange | orchid | peru | pink | plum | purple | red | salmon | sienna | silver | snow | tan | teal | thistle | tomato | turquoise | violet | white | yellow ;";
+  if ("webkitSpeechRecognition" in window) {
+    let recognition = new webkitSpeechRecognition();
+    return recognition;
+  } else {
+    console.log("Speech recognition not available");
+  }
+}
