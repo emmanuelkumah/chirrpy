a5855e1604b6e013f2aef80c08c7d14a4e317cfd
Emmanuel Kumah <mailkumah@gmail.com>
2023-10-13 13:37:42 +0000

    Added ability to download quill editor content as pdf

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,27 +1,44 @@
-import React, { useState, useEffect } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import { useOutletContext } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+//import ReactQuill from "react-quill";
+//import "react-quill/dist/quill.snow.css";
+import Quill from "quill";
+
+import { pdfExporter } from "quill-to-pdf";
 
 const Editor = () => {
   const storedContent = localStorage.getItem("details");
   const [editContent, setEditContent] = useState(storedContent);
+  const [editor, setEditor] = useState(null);
+
+  useEffect(() => {
+    const quillEditor = new Quill("#quill-editor", {
+      modules: {
+        toolbar: [
+          [{ header: [1, 2, false] }],
+          ["bold", "italic", "underline"],
+          ["image", "code-block"],
+        ],
+      },
+      placeholder: "Compose an epic...",
+      theme: "snow", // or 'bubble'
+    });
+    setEditor(quillEditor);
+  }, []);
 
   //update the content in local storage
   useEffect(() => {
     localStorage.setItem("details", editContent);
   }, [editContent]);
 
-  console.log(editContent);
-
+  const generatePDF = async () => {
+    const pdfBlob = await pdfExporter.generatePdf(editor.getContents());
+    const pdfUrl = URL.createObjectURL(pdfBlob);
+    window.open(pdfUrl);
+  };
   return (
     <>
-      <p>{editContent}</p>
-      <ReactQuill theme="snow" value={editContent} onChange={setEditContent} />
-      <div className="flex gap-3 mt-7">
-        <button className="bg-red-400 py-3">Save Content</button>
-        <button>Download</button>
-      </div>
+      <div id="quill-editor">{editContent}</div>
+      <button onClick={generatePDF}>Generate PDF</button>
     </>
   );
 };
